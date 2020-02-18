d465594c033b75d5947fb92a0f60bb2eda396577
sameerbairwa <sameerbairwa07@gmail.com>
2020-02-18 21:46:59 +0530

    updates

--- 06multerupload/app.js
+++ 06multerupload/app.js
@@ -6,12 +6,47 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+//multer setting 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/myupload')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+  })
+   
+var upload = multer({ 
+    storage: storage,
+    
+ }).single("profilepic");
+
 // set for ejs
 app.set("view engine", "ejs");
-app.get("/", (req, res) => {
-    res.send('multer is going fine with nodemon...');
-})
+// static folder
+app.use(express.static('./public'));
+
+// app.get("/", (req, res) => {
+//     res.send('multer is going fine with nodemon...');
+// })
 app.get("/", (req,res) => {
     res.render('index');
 })
+
+app.post('/upload', (req, res) => {
+    upload(req, res, error => {
+        if(error){
+            res.render('index', {
+                message: error
+            });
+        }else{
+            res.render('index',{
+                message:"successfully uploaded",
+                filename: `myupload/${req.file.filename}`
+            });
+        } 
+    }); 
+} );
+
 app.listen(port, () => console.log(`server is running at: ${port}...`));
